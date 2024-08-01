72bfcd179b03a7bfba4048fe4c886d507fdf7be8
josetetevi95 <jose.tetevi@gmail.com>
2024-08-01 15:17:12 +0200

    [FIXED] :  Import Url in backend urks

--- backend/urls.py
+++ backend/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
+    path('api/', include('profiles.urls')),
     path('api/email/', include('email_app.urls')),
     path('', include('backoffice.urls')),
 ]
